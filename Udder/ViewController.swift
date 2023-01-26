ff3aa6b4c289c8797a95b62d3f2b9b7cbd0b806e
silaa-topal <silaatopal@gmail.com>
2023-01-26 09:29:13 +0300

    disappear is added to view controller

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         <#code#>
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
 
 
 }
